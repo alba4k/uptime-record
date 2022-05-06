c765d508628dfcc4580ab09275166b7c91dafaac
alba4k <blaskoazzolaaaron@gmail.com>
2022-05-06 16:31:18 +0200

    --background

--- Makefile
+++ Makefile
@@ -1,10 +1,8 @@
 uptime-record: uptime-record.c
 	touch ~/.config/uptime-record
-	gcc uptime-record.c -ouptime-record
-	touch ~/.config/uptime-record
+	gcc uptime-record.c -ouptime-record -Wall
 
-install: uptime-record.c uptime-record
-	touch ~/.config/uptime-record
+install: uptime-record
 	cp uptime-record /usr/bin/uptime-record
 
 uninstall:
--- uptime-record.c
+++ uptime-record.c
@@ -3,11 +3,16 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <unistd.h>
+#include <limits.h>
+
+#define DEFAULT_TIME 300
 
 bool is_silent = false;
 bool asking_help = false;
 bool user_is_an_idiot = false;
 char *color = "\e[32m";
+int time = DEFAULT_TIME;
 
 void uptime(long uptime) {      // prints the uptime
     long days = uptime/86400;
@@ -16,7 +21,7 @@ void uptime(long uptime) {      // prints the uptime
     int sec = uptime - days*86400 - hours*3600 - mins*60;
 
     if(days) {
-        printf("%dd ", days);  // print the number of days passed if more than 0
+        printf("%ldd ", days);  // print the number of days passed if more than 0
     }
     if(hours) {
         printf("%dh ", hours); // print the number of days passed if more than 0
@@ -28,6 +33,7 @@ void uptime(long uptime) {      // prints the uptime
         printf("%ds", sec);    // print the number of seconds passed if more than 0
     }
 }
+
 
 
 int main(const int argc, const char **argv) {
@@ -65,34 +71,71 @@ int main(const int argc, const char **argv) {
                 user_is_an_idiot = true;
             }
         }
+        else if(!strcmp("-b", argv[i]) || !strcmp("--background", argv[i])) {
+            if(i < argc - 1) {
+                time = atoi(argv[i+1]) ? atoi(argv[i+1]) : DEFAULT_TIME;
+            }
+            struct sysinfo info;
+            sysinfo(&info);
+
+            unsigned long current;
+            char best[16];
+            char path[LOGIN_NAME_MAX + 32]; // login name + space for "/home/" and "/.config/uptime-record"
+            snprintf(path, 64, "%s/.config/uptime-record", getenv("HOME"));
+
+            FILE *fp = fopen(path, "r");
+            if(!fp)
+                return 69; // file didn't open correctly
+            fclose(fp);
+            
+            while(true) {
+                sysinfo(&info);
+                current = info.uptime;
+
+                fp = fopen(path, "r+");
+                fgets(best, 10, fp);
+                rewind(fp);
+                
+                if(current > atol(best)) {
+                    fprintf(fp, "%ld\n", current);
+                    printf("Updating %s to %ld from %s\n", path, current, best);
+                }
+                fclose(fp);
+                sleep(time);
+            }
+        }
     }
 
-    if(user_is_an_idiot) return 1; // you fucking idiot
+    if(user_is_an_idiot)
+        return 1; // you fucking idiot
 
     if(asking_help) {
-        printf("Keep track of your highest uptime!\n", color);
+        printf("Keep track of your highest uptime!\n");
         printf("\n%sFLAGS\e[0m:\n", color);
         printf("\t%s-h\e[0m, %s--help\e[0m:\tPrint this help menu and exit\n", color, color);
-        printf("\t%s-c\e[0m,%s --color\e[0m:\t Change the output color (default: green) [black, red, green, yellow, blue, purple, cyan, shell]\n", color, color);
+        printf("\t%s-c\e[0m,%s --color\e[0m:\t Change the output color (default: green)\n"
+               " [\e[30mblack\e[0m, \e[31mred\e[0m, \e[32mgreen\e[0m, \e[33myellow\e[0m, \e[34mblue\e[0m,"
+               " \e[35mpurple\e[0m, \e[36mcyan\e[0m, \e[39mwhite\e[0m]\n", color, color);
         printf("\t%s-s\e[0m, %s--silent\e[0m:\tDon't print any output\n", color, color);
+        printf("\t%s-b\e[0m, %s--background\e[0m:\tKeep active (default: 300) [refresh seconds]\n", color, color);
         return 0;
     }
     struct sysinfo info;
     sysinfo(&info);
 
     unsigned long current = info.uptime;
-
+    char best[16];
     char path[64];
+
     snprintf(path, 64, "%s/.config/uptime-record", getenv("HOME"));
 
     FILE *fp = fopen(path, "r+");
     if(!fp) {return 69;} // file didn't open correctly
     
-    char best[10];
     fgets(best, 10, fp);
     rewind(fp);
     
-    if (current > atol(best)) {
+    if(current > atol(best)) {
         if(!is_silent) {
             printf("Current uptime: ");
             uptime(current);
